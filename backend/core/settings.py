d101dcf4e35a386ff8d3149378682af2265cfaac
mohabroha@mail.de <mohabroha@mail.de>
2026-07-03 14:56:23 +0200

    Update legal pages and configure CORS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -36,10 +36,12 @@ INSTALLED_APPS = [
     "orders_app.apps.OrdersAppConfig",
     "reviews_app.apps.ReviewsAppConfig",
     "base_info_app.apps.BaseInfoAppConfig",
+    "corsheaders",
 ]
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,3 +126,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 6,
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
